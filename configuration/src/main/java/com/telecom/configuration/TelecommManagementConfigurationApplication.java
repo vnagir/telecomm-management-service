399d2f68a72f975f4ebea6531e526e85d969c3cb
Vinay Kumar Reddy Nagireddy <vnagireddy@securonix.com>
2021-05-19 00:16:16 -0500

    Initial commit

---
+++ configuration/src/main/java/com/telecom/configuration/TelecommManagementConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.telecom.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecommManagementConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecommManagementConfigurationApplication.class, args);
+	}
+
+}
---
+++ configuration/src/test/java/com/telecom/configuration/TelecommManagementConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecom.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecommManagementConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
